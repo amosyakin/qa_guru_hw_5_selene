f756a6c15c0c30f43dbc6318ce73af3666b48200
Aleksandr Mosyakin <aleks.mosyakin@gmail.com>
2024-03-25 22:27:17 +0300

    fix test 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,9 @@ from selene import browser
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_management():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-    browser.config.driver_name = 'chrome'
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 800
+    browser.config.window_height = 600
 
     yield
 
