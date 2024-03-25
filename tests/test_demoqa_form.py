ccb35f02d2bb8b9670c1b0eb89e1e9dfc9c617a9
Aleksandr Mosyakin <aleks.mosyakin@gmail.com>
2024-03-25 21:13:23 +0300

    fix test

--- tests/test_demoqa_form.py
+++ tests/test_demoqa_form.py
@@ -1,5 +1,5 @@
 import os.path
-from selene import browser, have, command
+from selene import browser, have, command, by
 
 
 def test_submit_form():
@@ -8,20 +8,24 @@ def test_submit_form():
     browser.element('#userName-wrapper').element('#firstName').type('Ivan')
     browser.element('#userName-wrapper').element('#lastName').type('Ivanov')
     browser.element('#userEmail-wrapper').element('#userEmail').type('ivanivanov@mail.com')
-    browser.element('#genterWrapper').element('#gender-radio-1').perform(command.js.click)
+    browser.element('#genterWrapper').element(by.text('Male')).perform(command.js.click)
     browser.element('#userNumber-wrapper').element('#userNumber').type('9876543210')
-    browser.element('#dateOfBirth-wrapper').click()
-    browser.element('.react-datepicker__month-dropdown-container').click().element('[value="0"]').click()
-    browser.element('.react-datepicker__year-select').click().element('[value="2000"]').click()
-    browser.element('.react-datepicker__day--001.react-datepicker__day--weekend').click()
-    # browser.element('#subjectsWrapper').click().element('#subjectsContainer').type('e')
-    browser.element('#hobbiesWrapper').element('#hobbies-checkbox-3').perform(command.js.click)
+    browser.element('#dateOfBirth-wrapper').element('#dateOfBirthInput').perform(command.js.click)
+    browser.element('.react-datepicker__month-select').click().element(by.text('January')).click()
+    browser.element('.react-datepicker__year-select').click().element(by.text('2000')).click()
+    browser.element('.react-datepicker__week').element(by.text('1')).click()
+    browser.element('#subjectsWrapper').element('#subjectsInput').type('en')
+    browser.element(by.text('English')).click()
+    browser.element('#hobbiesWrapper').element(by.text('Music')).perform(command.js.click)
     browser.element('#uploadPicture').send_keys(os.path.abspath('picture.jpg'))
     browser.element('#currentAddress-wrapper').element('#currentAddress').type('Red Square, 1')
-    browser.element('#stateCity-wrapper').element('#state').click().element('#react-select-3-option-1').click()
-    browser.element('#stateCity-wrapper').element('#city').click().element('#react-select-4-option-2').click()
+    browser.element('#stateCity-wrapper').perform(command.js.scroll_into_view)
+    browser.element('#stateCity-wrapper').element('#state').click().element(by.text('Haryana')).click()
+    browser.element('#stateCity-wrapper').element('#city').click().element(by.text('Karnal')).click()
     browser.element('#submit').click()
 
     browser.element('.modal-header>div').should(have.text('Thanks for submitting the form'))
-    # browser.element('.modal-body').should(have.css_class('table-responsive'))
+    browser.element('.table').all('td').even.should(have.exact_texts(
+        'Ivan Ivanov', 'ivanivanov@mail.com', 'Male', '9876543210', '01 January,2000',
+        'English', 'Music', 'picture.jpg', 'Red Square, 1', 'Haryana Karnal'))
     browser.element('.modal-footer>button').should(have.text('Close'))
