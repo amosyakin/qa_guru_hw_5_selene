011c89748c72a6c8b72a4d4e95403358c968c301
Aleksandr Mosyakin <aleks.mosyakin@gmail.com>
2024-04-12 22:09:24 +0300

    Mid level step objects

---
+++ qa_gure_5_hw_selene/resources.py
@@ -0,0 +1,7 @@
+import os
+
+
+def path(file_name):
+    return str(
+        os.path.abspath(file_name)
+    )
--- tests/test_demoqa_form.py
+++ tests/test_demoqa_form.py
@@ -1,31 +1,35 @@
-import os.path
-from selene import browser, have, command, by
+from qa_gure_5_hw_selene.model.pages.registration_page import RegistrationPage
 
 
 def test_submit_form():
-    browser.open('/automation-practice-form')
+    registration_page = RegistrationPage()
+    registration_page.open()
 
-    browser.element('#firstName').type('Ivan')
-    browser.element('#lastName').type('Ivanov')
-    browser.element('#userEmail').type('ivanivanov@mail.com')
-    browser.element(by.text('Male')).perform(command.js.click)
-    browser.element('#userNumber').type('9876543210')
-    browser.element('#dateOfBirthInput').perform(command.js.click)
-    browser.element('.react-datepicker__month-select').click().element(by.text('January')).click()
-    browser.element('.react-datepicker__year-select').click().element(by.text('2000')).click()
-    browser.element('.react-datepicker__week').element(by.text('1')).click()
-    browser.element('#subjectsInput').type('en')
-    browser.element(by.text('English')).click()
-    browser.element(by.text('Music')).perform(command.js.click)
-    browser.element('#uploadPicture').send_keys(os.path.abspath('picture.jpg'))
-    browser.element('#currentAddress').type('Red Square, 1')
-    browser.element('#state').perform(command.js.scroll_into_view)
-    browser.element('#state').click().element(by.text('Haryana')).click()
-    browser.element('#city').click().element(by.text('Karnal')).click()
-    browser.element('#submit').click()
+    registration_page.fill_first_name('Ivan')
+    registration_page.fill_last_name('Ivanov')
+    registration_page.fill_user_email('ivanivanov@mail.com')
+    registration_page.fill_user_gender('Male')
+    registration_page.fill_user_number('9876543210')
+    registration_page.fill_date_of_birth(year='2000', month='January', day='1')
+    registration_page.input_subjects(short_value='en', full_value='English')
+    registration_page.select_hobbies('Music')
+    registration_page.upload_picture('picture.jpg')
+    registration_page.fill_address('Red Square, 1')
+    registration_page.select_state('Haryana')
+    registration_page.select_city('Karnal')
+    registration_page.click_submit_button()
 
-    browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
-    browser.element('.table').all('td').even.should(have.exact_texts(
-        'Ivan Ivanov', 'ivanivanov@mail.com', 'Male', '9876543210', '01 January,2000',
-        'English', 'Music', 'picture.jpg', 'Red Square, 1', 'Haryana Karnal'))
-    browser.element('#closeLargeModal').should(have.text('Close'))
+    registration_page.should_finish_form_title('Thanks for submitting the form')
+    registration_page.should_registrated_user_with(
+        'Ivan Ivanov',
+        'ivanivanov@mail.com',
+        'Male',
+        '9876543210',
+        '01 January,2000',
+        'English',
+        'Music',
+        'picture.jpg',
+        'Red Square, 1',
+        'Haryana Karnal'
+    )
+    registration_page.should_finish_form_button('Close')
