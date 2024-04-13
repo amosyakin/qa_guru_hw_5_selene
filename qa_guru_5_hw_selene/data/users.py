70cdb5c877e3393a2c7c69337cb8eb98dcabcdd3
Aleksandr Mosyakin <aleks.mosyakin@gmail.com>
2024-04-13 20:49:17 +0300

    High level step objects

---
+++ qa_guru_5_hw_selene/data/users.py
@@ -0,0 +1,28 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class User:
+    first_name: str
+    last_name: str
+    email: str
+    gender: str
+    phone_number: str
+    birth_day: str
+    birth_month:str
+    birth_year: str
+    subjects: str
+    hobbies: str
+    picture_path: str
+    address: str
+    state: str
+    city: str
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def state_and_city(self):
+        return f'{self.state} {self.city}'
+
+    def date_of_birth(self):
+        return f'{self.birth_day} {self.birth_month}, {self.birth_year}'
